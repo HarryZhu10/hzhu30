f8dbf3de7e590875fbb7d6f39204487746135573
HarryZhu231227919 <hzhu30@stuy.edu>
2022-11-23 13:59:10 -0500

    update

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -6,16 +6,20 @@ K20 -- REST API
 time spent
 """
 
-from flask import flask
+from flask import Flask
 from flask import render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def display():
     return render_template('main.html')
 
+with open('key_nasa.txt') as f:
+    key = f.read()
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
+
