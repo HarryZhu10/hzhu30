9e8d3ee71eb2def80f93066fadd8f7496a0c3c0c
Harry <hzhu30@stuy.edu>
2022-11-23 00:06:15 -0500

    update

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -6,14 +6,22 @@ K20 -- REST API
 time spent
 """
 
-from flask import flask
+from flask import Flask
 from flask import render_template
+import requests
 
-app = flask(__name__)
+app = Flask(__name__)
+
+key = open("key_nasa.txt").read()
+urlString = "https://api.nasa.gov/planetary/apod?api_key=" + key
+res = requests.get(urlString)
+json = res.json()
+#print(res.text) #str type
+#print(json["url"])
 
 @app.route("/")
-def display():
-    return render_template('main.html')
+def picture():
+    return render_template('main.html', url = json["url"], explanation = json["explanation"], title = json["title"])
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
