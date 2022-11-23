599d1c115e8692910f762de4144320ec0821569b
HarryZhu231227919 <hzhu30@stuy.edu>
2022-11-23 14:01:54 -0500

    update

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -1,3 +1,4 @@
+
 """
 Space Butterflies: Gordon Mo, Harry Zhu
 Softdev
@@ -11,8 +12,6 @@ from flask import render_template
 import requests
 
 app = Flask(__name__)
-<<<<<<< HEAD
-=======
 
 key = open("key_nasa.txt").read()
 urlString = "https://api.nasa.gov/planetary/apod?api_key=" + key
@@ -20,17 +19,12 @@ res = requests.get(urlString)
 json = res.json()
 #print(res.text) #str type
 #print(json["url"])
->>>>>>> 9e8d3ee71eb2def80f93066fadd8f7496a0c3c0c
 
 @app.route("/")
 def picture():
     return render_template('main.html', url = json["url"], explanation = json["explanation"], title = json["title"])
 
-with open('key_nasa.txt') as f:
-    key = f.read()
-
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
-
